b9421b6a71e6e5fc09161e1a20427f73712ebd40
claudioCMW <72928953+claudioCMW@users.noreply.github.com>
2021-05-10 10:20:51 -0300

    modulo jsII realizado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -156,20 +156,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-
-  if((num1>num2) && (num1>num3) && (num1>0)){
-     return "Número 1 es mayor y positivo"
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
   }
-  if(((num1<0)) || (num2<0) || (num3<0)){
-       return  "Hay negativos";
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
   }
-  if ((num3>num1)&&(num3>num2)){
-     return num3+1;
+  if (num3 > num1 && num3 > num2) {
+    num3++;
+    return num3;
   }
-  if ((num3*num2*num1)===0){
-      return "Error";
+  if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Número 1 es mayor y positivo";
   }
-   return false;
+  else {
+    return false;
+  }
 }
 //__________________________________________________________________________________________
 function esPrimo(numero) {
@@ -179,17 +181,23 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   var int =Math.floor((Math.sqrt(numero)));
-  var boolean =false;
-  for(let n=2;n<int;n++){
+  var boolean =true;
+  if(numero===0 || numero===1){
+      return false
+  }else{
+  for(var n=2;n<int;n++){
+      
       if(numero%n===0){
-          boolean=true;
+          boolean=false
 
       }
   }
+}
   if (boolean){
-     return false;
+     return true
   }
-  return true;
+     return false
+  
 }
 //_________________________________________________________________________________________
 function esVerdadero(valor){
@@ -206,19 +214,23 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  var array = new Array(10);
+  var array = [];
   var n1=6;
   for (var n=0;n<=10;n++){
   
        array.push(n*n1);
      
   }
-  return  array.values;
+  return  array;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if (numero.toString().length===3){
+    return true
+  }
+  return false;
   
 }
 
@@ -226,6 +238,12 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var n=1;
+  do{
+     numero +=5;
+     n+=1;
+  }while (n<=8)
+  return numero;
 }
 
 
